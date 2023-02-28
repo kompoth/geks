a1ac5bf3c31a3cc2682420cd701803c3b4ce91aa
Vasily Negrebetskiy <kmiziz.com@gmail.com>
2023-02-28 10:20:06 +0300

    docstring for hexmap

--- geks/hexmap.py
+++ geks/hexmap.py
@@ -5,11 +5,27 @@ from .hexagon import Hex
 from .geometry import hex_circle
 
 class Hexmap:
-    """Class for a space of hexagons."""
-    def __init__(
-        self, default, radius=None, dims=None, 
-        rect=True, flat=True
-    ):
+    """Base data structure for a collection of hexagons."""
+    def __init__(self, default, radius=None, dims=None, flat=True):
+        """
+        Constructor. Can build a hexagonal (round) or rectangular map.
+        map.
+
+        Parameters
+        ----------
+        default : any
+            Any piece of data that should be stored in a hexagon by
+            default.
+        radius : int, default: None
+            Radius of a hexagonal map. If not None, a round map will be
+            built. One of 'radius' or 'dims' must be provided.
+        dims : iterable(int), default: None
+            Lengths of angled map edges. If not None, a rectangular map
+            will be built. One of 'radius' or 'dims' must be provided.
+        flat : bool, default: True
+            If True, map is constructed for flat top haxagons. Neccessary
+            only for rectangular maps.
+        """
         self.map = {}
         if radius:
             zero = Hex((0, 0))
