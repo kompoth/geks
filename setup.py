be6d3ddfa3a4fa13458912fe6e8a02425ea25381
Vasily Negrebetskiy <kmiziz.com@gmail.com>
2023-08-31 14:58:56 +0400

    fixing release wf

--- setup.py
+++ setup.py
@@ -3,18 +3,24 @@ import os
 from setuptools import Extension, setup
 import numpy
 
+try:
+    from Cython.Build import cythonize
+    HAS_CYTHON = True
+except ImportError:
+    HAS_CYTHON = False
+
 USE_CYTHON = bool(int(os.getenv("USE_CYTHON", 0)))
-if USE_CYTHON:
-    try:
-        from Cython.Build import cythonize
-    except ImportError:
-        if USE_CYTHON:
-            raise
 
 if USE_CYTHON:
     src_file = os.path.join("geks", "cython", "fill_sinks.pyx")
 else:
     src_file = os.path.join("geks", "cython", "fill_sinks.c")
+    if not os.path.exists(src_file):
+        src_file = os.path.join("geks", "cython", "fill_sinks.pyx")
+        USE_CYTHON = True
+
+if USE_CYTHON and not HAS_CYTHON:
+    raise ImportError("No module named 'Cython'")
 
 extensions = [Extension("geks.cython.fill_sinks", [src_file])]
 
