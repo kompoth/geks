72c682fb108fcbd44b8129aa329740864ac1ee83
Vasily Negrebetskiy <kmiziz.com@gmail.com>
2023-09-02 00:29:13 +0400

    flake8

--- test/test_rivers.py
+++ test/test_rivers.py
@@ -1,16 +1,15 @@
 import geks
-from geks.front import FrontMPL
 
 
 def test_river_centers():
     hm = geks.RectHexmap(None, (40, 20))
     geks.gen_heightmap(hm)
     geks.fill_sinks_cy(hm)
-    rivers = geks.generate_rivers(hm, 5, -1, 150, edgify=False)
+    geks.generate_rivers(hm, 5, -1, 150, edgify=False)
 
 
 def test_river_edges():
     hm = geks.RectHexmap(None, (40, 20))
     geks.gen_heightmap(hm)
     geks.fill_sinks_cy(hm)
-    rivers = geks.generate_rivers(hm, 5, -1, 150)
+    geks.generate_rivers(hm, 5, -1, 150)
